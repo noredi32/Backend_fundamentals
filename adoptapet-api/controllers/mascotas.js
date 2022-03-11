61818d85ae4d5507ad3b91e6238e7e5b1f10e013
noredi32 <noredi@gmail.com>
2022-03-10 19:15:34 -0600

    10.03.2022

--- adoptapet-api/controllers/mascotas.js
+++ adoptapet-api/controllers/mascotas.js
@@ -1,25 +1,28 @@
 const Usuario = require('../models/Mascota')
 
 function crearMascota(req, res) {
+  // Instanciaremos un nuevo usuario utilizando la clase usuario
   var mascota = new Mascota(req.body)
-  res.status(201).send(mascota)
+  res.status(201).send(usuario)
 }
 
 function obtenerMascotas(req, res) {
-  var mascota1 = new Mascota(1, 'Mascota', 'Vega', 'juan@vega.com')
-  var mascota2 = new Mascota(2, 'Mascota2', 'Vega', 'mon@vega.com')
+  // Simulando dos usuarios y respondiendolos
+  var mascota1 = new Mascota(1, 'Canito', 'Vega', 'juan@vega.com')
+  var mascota2 = new Mascota(2, 'Canito2', 'Vega', 'mon@vega.com')
   res.send([mascota1, mascota2])
 }
 
 function modificarMascota(req, res) {
-  var mascota1 = new Mascota(req.params.id, 'Juan', 'Vega', 'juan@vega.com')
+  // simulando un usuario previamente existente que el usuario utili
+  var mascota1 = new Mascota(req.params.id, 'Rufo', 'Vega', 'juan@vega.com')
   var modificaciones = req.body
   mascota1 = { ...mascota1, ...modificaciones }
   res.send(mascota1)
 }
 
 function eliminarMascota(req, res) {
-  res.status(200).send(`Lomito ${req.params.id} eliminado`);
+  res.status(200).send(`Mascota ${req.params.id} eliminado`);
 }
 
 module.exports = {
